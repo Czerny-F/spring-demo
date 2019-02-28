30dc8e24986754b544eb43e16ba48ee742976b20
Czerny-F <dummy00.cz@gmail.com>
2019-03-01 01:00:41 +0900

    rest post

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,13 @@ public class MainController {
   public Map<String, Object> index() {
     var res = new HashMap<String, Object>();
     res.put("hello", "world");
+    return res;
+  }
+
+  @PostMapping(value = "/")
+  public Map<String, Object> post() {
+    var res = new HashMap<String, Object>();
+    res.put("result", "ok");
     return res;
   }
 }
